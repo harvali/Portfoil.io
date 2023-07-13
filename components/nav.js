4594bd6548cd00a6c8a2248fd1b42884a6ffa220
harvali <harri.valilae@hotmail.com>
2023-07-12 20:09:08 -1000

    TODO: updated cutom components

---
+++ components/nav.js
@@ -0,0 +1,29 @@
+class Nav extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+  connectedCallback() {
+    this.innerHTML = `
+        <div class="nav-brand">
+            <a href="#">Portfoil.io</a>
+        </div>
+        <input id="menu-toggle" type="checkbox" />
+        <label class='menu-button-container' for="menu-toggle">
+        <div class='menu-button'></div>
+        </label>
+        <ul class="navbar-nav">   
+            <li class="nav-item"><a class="nav-link" href="index.html"><i class="fa fa-home fa-2x fa-fw"></i> Home</a> </li>
+            <li class="nav-item"><a class="nav-link" href="cv.html"><i class="fa fa-file-text fa-2x fa-fw"></i> Resume</a></li>
+            <li class="nav-item"><a class="nav-link" href="projects.html"><i class="fa fa-cog fa-2x fa-fw"></i> Projects</a></li>
+            <li class="nav-item"><a class="nav-link" href="blog.html"><i class="fa-solid fa-blog fa-2x fa-fw"></i> Blog</a></li>
+            <li class="nav-item"><a class="nav-link" href="https://github.com/harvali"><i class="fa fa-github fa-2x fa-fw"></i> Github</a></li>
+            <li class="nav-item"><a class="nav-link" href="https://www.linkedin.com/in/harvali/"><i class="fa fa-linkedin-square fa-2x fa-fw"></i> LinkedIn</a></li>
+        </ul>
+    `;
+  }
+}
+
+
+
+  customElements.define('nav-component', Nav);
